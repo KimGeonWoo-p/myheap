a125b3f0feb9e077a89cbdc1cbd79e190b0171cf
KimGeonWoo-p <76949011+KimGeonWoo-p@users.noreply.github.com>
2024-01-12 15:17:01 +0900

    Update avl_heap.c
    2024-01-12

--- avl_heap.c
+++ avl_heap.c
@@ -33,14 +33,59 @@ int append(tnode* node, int value)
 //pop
 //가장 작은 값을 heap에서 제거 후 반환
 //제거하는 도중 rotate실시.
-int pop(theap* heap);
+int pop(tnode* node)
+{
+    if (!node->left)
+    {
+        int result = node->value;
+        node->parent->left = NULL;
+        free(node->left);
+        return result;
+    }
+
+    return pop(node->left);
+}
 
 //rotate
-void ll_rotate(theap* heap);
-void rr_rotate(theap* heap);
-void rl_rotate(theap* heap);
-void lr_rotate(theap* heap);
+void ll_rotate(theap* heap)
+{
+
+}
+
+void rr_rotate(theap* heap)
+{
+
+}
+
+void rl_rotate(theap* heap)
+{
+
+}
+
+void lr_rotate(theap* heap)
+{
+
+}
+
+int get_height(tnode* node)
+{
+    if (!node)
+        return 0;
+
+    int result = 1;
+    if (node->left)
+        result = get_height(node->left) + 1;
+    if (node->right)
+        result = max(result, get_height(node->right) + 1);
+    return result;
+}
 
 //height-balance property을 업데이트하는 함수
-//메모이제이션을 통한 구현.
-void get_hbp(tnode* node);
+int get_hbp(tnode* node)
+{
+    //node가 null인 경우
+    if (!node)
+        return -1;
+    
+    return get_height(node->left) - get_height(node->right);
+}
